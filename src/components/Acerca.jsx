62f6cf21c9ff838f9d5466847cb0f7965b3d8581
juan campos <juan.camposluke@gmail.com>
2023-06-07 22:33:31 -0600

    yeah.

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -15,12 +15,12 @@ function Acerca() {
       <p>
         Puedes descargar mi currículum para obtener más detalles sobre mi formación académica y experiencia laboral.
       </p>
-      <a className="me" href="https://drive.google.com/file/d/1IQgbs0iJNeSrOA1Dqpu46Jo3TkDgZtq2/view?usp=drive_link" target="_blank" download>Descargar currículum</a>
+      <a className="me" href="https://drive.google.com/file/d/1IQgbs0iJNeSrOA1Dqpu46Jo3TkDgZtq2/view?usp=drive_link" target="_blank" rel="noopener noreferrer" download>Descargar currículum</a>
       <br /><br />
       <p>
         Tambien puedes descargar mi currículum acerca de los conocimientos adquiridos en el area de tecnologia y sobre mi formacion en la academia de Kodigo.
       </p>
-      <a className="me" href="https://drive.google.com/file/d/1208CHpom2GowPHMa24sHZ_3STak5OWVU/view?usp=drive_link" target="_blank" download>Descargar plantilla de Kodigo</a>
+      <a className="me" href="https://drive.google.com/file/d/1208CHpom2GowPHMa24sHZ_3STak5OWVU/view?usp=drive_link" target="_blank" rel="noopener noreferrer" download>Descargar plantilla de Kodigo</a>
     </div>
    
   );
